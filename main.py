bb80e4603a5faff170495e37014554cee7e7817f
cbwgit <73323721+cbwgit@users.noreply.github.com>
2022-01-09 15:46:55 +0800

    Add files via upload
    Add web server and control method

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+from importlib import import_module
+from flask import Flask, render_template, Response ,request
 import time
 import RPi.GPIO as GPIO
 from pump import PumpWater
@@ -9,14 +12,36 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(32, GPIO.IN)         #Water read
 GPIO.setup(3, GPIO.IN)
 
-output,Moisture=MoistureDetect()
+moistureContent=0.8
 
-if output<0.9:
-    water = WaterPredict().predict([[output]])
-    print(water)
-    lineMessage('hiyaku kite') 
-    PumpWater(water[0])
-else:
-    lineMessage('no need ')
+def moisture(moistureContent):
+    output,Moisture=MoistureDetect()
+    if output<(moistureContent/100):
+        water = WaterPredict().predict([[output]])
+        print(water)
+        lineMessage('hiyaku kite') 
+        PumpWater(water[0])
+    else:
+        lineMessage('no need ')
 
-time.sleep(5)
+# Flask constructor
+
+app = Flask(__name__)
+
+
+
+@app.route('/',methods=['POST','GET'])
+def index():
+    #Get moisture value
+    if request.method == 'POST':
+      moistureContent = request.form["moistureContent"]
+      moisture(int(moistureContent))
+      return moistureContent
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.debug=True
+    app.run(host='0.0.0.0', threaded=True)
+
+
